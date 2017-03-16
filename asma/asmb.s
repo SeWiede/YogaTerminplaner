d1a84829995571761de195f6b446f12d8b70020b
Sebastian Michael Wiedemann <u1425647@g0.complang.tuwien.ac.at>
2017-03-16 16:27:54 +0100

    asmb geht

--- asma/asmb.s
+++ asma/asmb.s
@@ -10,25 +10,45 @@ asmb:
 	#frei: rax, r8, r9, r10, r11
 		
 	#y saven 
-;	mov %rdx, %r8	
-;	mov %rcx, %r11
-;	mov %rsi, %rcx
+	mov %rdx, %r8	
+	mov %rcx, %r11
+	mov %rsi, %rcx
 	
-;	mov $0, %rdx
+	mov $0, %rdx
+	mov $0, %r10
+	jecxz end
 test:	
-;	mov 0(%rdi), %rax
-;	mul %r8
+	mov 0(%rdi), %rax
+ 	mul %r8
+    add %r10, %rax
+	mov %rdx, %r10
+	adc $0, %r10
 	#rax = (0:16)
 	#rdx = (16:32)
-;	adc %rdx, %rax
-	;mov %rax, 0(%r11)
+	mov %rax, 0(%r11)
 
-	;lea 8(%r11, %r11, 1), %r11
-	;lea 8(%rdi, %rdi, 1), %rdi	
-;	loop test
+	#lea 8(%r11, %r11, 0), %r11
+	#lea 8(%rdi, %rdi, 0), %rdi
+	inc %r11	
+	inc %rdi	
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	inc %r11	
+	inc %rdi
+	loop test
 
-	;mov %rdx, 0(%r11)
-
+	mov %r10, 0(%r11)
+end:
 	ret
 
 	.cfi_endproc
